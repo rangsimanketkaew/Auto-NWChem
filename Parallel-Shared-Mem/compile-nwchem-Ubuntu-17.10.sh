084ee73e56b67010f54a53ba8ee6dec1906ec04b
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2018-03-28 12:37:31 +0800

    Create compile-nwchem-Ubuntu-17.10.sh

---
+++ Parallel-Shared-Mem/compile-nwchem-Ubuntu-17.10.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Ubuntu 17.10
+
+export NWCHEM_TOP=/home/nutt/nwchem-6.8
+export NWCHEM_TARGET=LINUX64
+export MAKE=/usr/bin/make
+export USE_NOFSCHECK=TRUE
+
+export PYTHONHOME=/usr
+export PYTHONVERSION=2.7
+export USE_PYTHON64=y
+export USE_PYTHONCONFIG=y
+export BLASOPT="-lopenblas -lpthread -lrt"
+export BLAS_SIZE=4
+export USE_64TO32=y
+
+export LD_LIBRARY_PATH=/usr/local/openmpi-1.8.1/lib/:$LD_LIBRARY_PATH
+export USE_MPI=y
+export USE_MPIF=y
+export USE_MPIF4=y
+export MPI_LOC=/usr/local/openmpi-1.8.1
+export MPI_LIB=/usr/local/openmpi-1.8.1/lib
+export MPI_INCLUDE=/usr/local/openmpi-1.8.1/include
+export LIBMPI="-lmpi_usempi -lmpi_mpifh -lmpi"
+
+#make -j4 clean
+make nwchem_config NWCHEM_MODULES="all python"
+make 64_to_32
+make
+
