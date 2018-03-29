1ccb406595108806c8ab8eeb212d4ab2593471a4
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2018-03-29 09:43:20 +0800

    Update and rename compile-nwchem-Ubuntu-17.10.sh to compile-nwchem-Ubuntu-OpenMPI.sh

--- Parallel-Shared-Mem/compile-nwchem-Ubuntu-OpenMPI.sh
+++ Parallel-Shared-Mem/compile-nwchem-Ubuntu-OpenMPI.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# NWChem 6.8 - Ubuntu 17.10
+# Bash script for compiling of NWChem on Ubuntu 17.10 using OpenMPI
+# Written by Rangsiman Ketkaew, MSc student in Chemistry, CCRU, Thammasat University, Thailand.
+
+# Compiling of NWChem 6.8 on Ubuntu 17.10.1 (Artful Aardvark)
 # Use NWCHEM's BLAS & OpenMPI 1.8.1
 # mpif90 -show = -lmpi_usempi -lmpi_mpifh -lmpi
 # The lines in MPI libraries box should be adjusted to satify the library/environment of your machine.
@@ -8,7 +11,6 @@ export NWCHEM_TOP=/home/nutt/nwchem-6.8
 export NWCHEM_TARGET=LINUX64
 export MAKE=/usr/bin/make
 export USE_NOFSCHECK=TRUE
-
 export PYTHONHOME=/usr
 export PYTHONVERSION=2.7
 export USE_PYTHON64=y
@@ -16,7 +18,6 @@ export USE_PYTHONCONFIG=y
 export BLASOPT="-lopenblas -lpthread -lrt"
 export BLAS_SIZE=4
 export USE_64TO32=y
-
 ## ---------------------- MPI Libraries box ---------------------------
 export LD_LIBRARY_PATH=/usr/local/openmpi-1.8.1/lib/:$LD_LIBRARY_PATH
 export USE_MPI=y
@@ -31,4 +32,3 @@ export LIBMPI="-lmpi_usempi -lmpi_mpifh -lmpi"
 make nwchem_config NWCHEM_MODULES="all python"
 make 64_to_32
 make
-
