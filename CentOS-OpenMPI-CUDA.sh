c86eced49940072e402448dfdb970caf7b0ad4bf
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2018-06-04 23:55:04 +0700

    Create CentOS-OpenMPI-CUDA.sh

---
+++ CentOS-OpenMPI-CUDA.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+##############################################################################################
+#  Program for NWChem compilation on CentOS with OpenMPI and CUDA compiled by gFrotran       #
+#  Nvidia CUDA architecture is used to accelerate TCE model                                  #
+#  Written by Rangsiman Ketkaew (MSc student in Chemistry), Thammasat University, Thailand.  #
+##############################################################################################
+
+# Install reprequisites:
+# yum install python-devel gcc-gfortran tcsh openssh-clients which
+# yum install openblas-devel openblas-serial64 openmpi-devel scalapack-openmpi-devel elpa-openmpi-devel 
+
+# Default OpenBLAS and Python 2.6 or 2.7 rpm-based are recommended.
+
+# Install the latest version of NVIDIA-CUDA, says /usr/local/cuda/
+# and append full path of 'nvcc' (cuda compiler) to $PATH in your .bashrc file.
+
+export NWCHEM_TOP=/builddir/build/BUILD/nwchem-6.8
+export NWCHEM_TARGET=LINUX64
+export CC=gcc
+export FC=gfortran
+export USE_ARUR=TRUE
+export USE_NOFSCHECK=TRUE
+export NWCHEM_FSCHECK=N
+export LARGE_FILES=TRUE
+export MRCC_THEORY=Y
+export EACCSD=Y
+export IPCCSD=Y
+export CCSDTQ=Y
+export CCSDTLR=Y
+export NWCHEM_LONG_PATHS=Y
+export PYTHONHOME=/usr
+export PYTHONVERSION=2.7
+export PYTHONLIBTYPE=so
+export USE_PYTHON64=y
+export HAS_BLAS=yes
+export BLASOPT="-L/usr/lib64 -lopenblas"
+export BLAS_SIZE=4
+export MAKE=/usr/bin/make
+export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib
+export USE_MPI=y
+export USE_MPIF=y
+export USE_MPIF4=y
+export MPIEXEC=/usr/lib64/openmpi/bin/mpiexec
+export MPI_LIB=/usr/lib64/openmpi/lib
+export MPI_INCLUDE=/usr/include/openmpi-x86_64
+export LIBMPI="-lmpi -lmpi_f90 -lmpi_f77"
+#------- GPU Enabled --------
+export TCE_CUDA=Y
+export CUDA_LIBS="-L/usr/local/cuda-9.1/lib64/ -L/usr/local/cuda-9.1/lib64/ -lcudart"
+export CUDA_FLAGS="-arch sm_50 "
+export CUDA_INCLUDE="-I. -I/usr/local/cuda-9.1/include/"
+
+$MAKE nwchem_config NWCHEM_MODULES="all python" 2>&1 | tee ../make_nwchem_config_openmpi.log
+$MAKE 64_to_32 2>&1 | tee ../make_64_to_32_openmpi.log
+export MAKEOPTS="USE_64TO32=y"
+$MAKE FC=gfortran CUDA=nvcc ${MAKEOPTS} 2>&1
